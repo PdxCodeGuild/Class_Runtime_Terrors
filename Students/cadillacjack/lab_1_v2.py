9e364784a383305ea75ea70a679503fecf999d84
CadillacJack42 <72709991+CadillacJack42@users.noreply.github.com>
2020-10-14 14:22:46 -0700

    Latest lab ver. Does not brek loop

--- Students/cadillacjack/lab_1_v2.py
+++ Students/cadillacjack/lab_1_v2.py
@@ -4,7 +4,42 @@
 y = 1
 n = 0
 
-initiate = input('''
+convert = {
+"ft": 0.03048,
+"mi": 1609.34,
+"m": 1.0,
+"km": 1000.0,
+"yard": 0.9144,
+"inch": 0.0254}
+
+
+
+def to_feet(user_distance, user_unit):
+    result = float(user_distance) * user_unit / convert["ft"]
+    print(f"The units you entered are equal to {result} Feet.")
+
+def to_miles(user_distance, user_unit):
+    result = float(user_distance) * user_unit / convert["mi"]
+    print(f"The units you entered are equal to {result} Miles.")
+
+def to_kilometers(user_distance, user_unit):
+    result = float(user_distance) * user_unit / convert["km"]
+    print(f"The units you entered are equal to {result} Kilometers.")
+
+def to_yards(user_distance, user_unit):
+    result = float(user_distance) * user_unit / convert["y"]
+    print(f"The units you entered are equal to {result} yards.")
+
+def to_inches(user_distance, user_unit):
+    result = float(user_distance) * user_unit / convert["i"]
+    print(f"The units you entered are equal to {result} inches.")
+
+def to_meters(user_distance, user_unit):
+    result = float(user_distance) * user_unit
+    print(f"The units you entered are equal to {result} meters.")
+
+def main():
+    initiate = input('''
 Welcome to Cadillac Jacks Unit Conversion Program.
 Written 10/13/2020
 
@@ -12,11 +47,11 @@ Would you like to convert a distance from
 one unit of measurement to another?
 
 Enter "y" for Yes or "n" for No - ''')
-while True:
-    if initiate.lower() == "y":
-        number = input("Enter the distance to be converted : ")
+    while True:
+        if initiate.lower() == "y":
+            number = input("Enter the distance to be converted : ")
 
-        initial_unit = input('''
+            initial_unit = input('''
 What is the starting unit of measurement?
 
 Enter "i" for Inches
@@ -28,7 +63,7 @@ Enter "km" for Kilometers
 
 Starting Unit - ''')
         
-        desired_unit = input('''
+            desired_unit = input('''
 How would you like the measurement to be displayed?
 
 Enter "i" for Inches
@@ -40,13 +75,36 @@ Enter "km" for Kilometers
 
 Final Unit - ''')
 
-        initiate = input('''
+        start = convert[initial_unit]
+
+        if desired_unit == "m":
+            to_meters(number, start)
+
+        elif desired_unit == "i":
+            to_inches(number, start)
+
+        elif desired_unit == "ft":
+            to_feet(number, start)
+
+        elif desired_unit == "y":
+            to_yards(number, start)
+
+        elif desired_unit == "mi":
+           to_miles(number, start)
+
+        elif desired_unit == "km":
+            to_kilometers(number, start)
+
+
+            initiate = input('''
 Would you like to make another conversion?
 Enter "y" for Yes or "n" to Exit - ''')
 
 
-    elif initiate.lower() == "n":
-        print("Thank you for using Cadillac Jacks Unit Conversion Program")
-        break
-    else:
-        print("Please enter 'y' for Yes or 'n' to Exit - ")
+        elif initiate.lower() == "n":
+            print("Thank you for using Cadillac Jacks Unit Conversion Program")
+            break
+        else:
+            print("Please enter 'y' for Yes or 'n' to Exit - ")
+
+main()
