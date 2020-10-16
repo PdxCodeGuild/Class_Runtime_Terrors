7464890c03dee96e742b5fc0f01d691b076b60bf
CadillacJack42 <72709991+CadillacJack42@users.noreply.github.com>
2020-10-15 21:30:43 -0700

    Version 3

---
+++ Students/cadillacjack/lab08/number_guess_v3.py
@@ -0,0 +1,34 @@
+import random
+
+
+initiate = input('''
+Would you like to play Guess the Number?
+Enter "y" for Yes, or "n" for No :  ''')
+
+secret = random.randint(1,10)
+
+counter = 0
+
+while initiate == "y":
+    guess = input('''
+Enter a number between 1 and 10
+Your Guess : ''')
+    
+    if int(guess) == secret:
+        print(f'''
+Congratulations! You have successfully guessed the secret number!
+You made {counter} attempts before successfully guessing the number. ''')
+        initiate = input('''
+Would You like to play again? 
+Enter "y" for Yes, or "n" for No :  ''')
+        counter = 0
+        secret = random.randint(1,10)
+    elif int(guess) != secret:
+        if int(guess) > secret:
+            print("Your guess was too high!")
+        elif int(guess) < secret:
+            print("Your guess was too low!")
+        counter += 1
+        print('''
+That is not the secret number. Try again...
+        ''')
