1e2601f21b6b83b69bcfbd58adabc1e857800784
Niu <ben.niu@accenture.com>
2020-10-17 00:34:05 -0400

    add version 5 to lab 08

--- Students/Ben/Lab08_Guess_the_number/Guess_Number.py
+++ Students/Ben/Lab08_Guess_the_number/Guess_Number.py
@@ -47,34 +47,43 @@
 #     else:
 #         print("Too low!")
 ####################################################
-# Version 4
-import random
-x = random.randint(1,10)
-print(x)
-guess = 0
-attempt = 1
+# # Version 4
+# import random
+# x = random.randint(1,10)
+# print(x)
+# guess = 0
+# attempt = 1
 
-while guess != x:
-    guess = int(input("Guess the number: "))
-    if guess == x:
-        print("Correct")
-        break
-    else: 
-        if attempt == 1:
-            print('This is your first attemp, no hisotrical comparison available')
-            attempt += 1
-            last_guess = guess 
-        else:
-            
-            print(f'your last guess is {last_guess}')
-            diff_now = abs(guess-x)
-            diff_prev = abs(last_guess-x)
-            if diff_now < diff_prev:
-                print("You are closer now")
-            else:
-                print("you are further now than your last guess")
-    last_guess = guess 
+# while guess != x:
+#     guess = int(input("Guess the number: "))
+#     if guess == x:
+#         print("Correct")
+#         break
+#     else: 
+#         if attempt == 1:
+#             print('This is your first attemp, no hisotrical comparison available')
+#             attempt += 1
+#             last_guess = guess 
+#         else:
+#             print(f'your last guess is {last_guess}')
+#             diff_now = abs(guess-x)
+#             diff_prev = abs(last_guess-x)
+#             if diff_now < diff_prev:
+#                 print("You are closer now")
+#             else:
+#                 print("you are further now than your last guess")
+#     last_guess = guess 
 ############################################################
 # Version 5
-        
+import random
+answer = int(input("Please enter the number you would like company to guess: "))
+guess = False
+while guess is False:
+    computer = random.randint(0,1000)
+    if computer == answer:
+        print(f"computer have successfully guessed your number which is {computer}")
+        guess = True
+    else:
+        print("Computer failed in the last guess, guessing a new number now")
+
         
