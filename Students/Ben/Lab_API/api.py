e3c9c1a5d659a9e100b3f7694c928861301b41ae
Ben <niu.41@osu.edu>
2020-10-21 22:49:21 -0400

    add book.text to the folder

--- Students/Ben/Lab_API/api.py
+++ Students/Ben/Lab_API/api.py
@@ -13,23 +13,46 @@
 # data = response.json()
 # print(data)
 
-import requests
+# import requests
 
-API_KEY = "8af2aa7fa978da0c3dc608a85406875c"
-API_URL = "http://api.openweathermap.org/data/2.5/weather"
-def getApi(loc):
-    ret = requests.get(f"{API_URL}?q={loc}&appid={API_KEY}")
-    return ret.json()
+# API_KEY = "8af2aa7fa978da0c3dc608a85406875c"
+# API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
-def main():
-    try:
-        loc = input("what city ")
-        ret = getApi(loc)
-        weather = ret["weather"][0]["description"]
-        temp = format(float(ret["main"]["temp"]) - 273.1, '.2f')
-        print(f"Its {temp} degrees in {loc} and {weather}")
-    except Exception:
-        print("error")
+# def getApi(loc):
+#     ret = requests.get(f"{API_URL}?q={loc}&appid={API_KEY}")
+#     return ret.json()
+
+# def main():
+#     try:
+#         loc = input("what city ")
+#         ret = getApi(loc)
+#         weather = ret["weather"][0]["description"]
+#         temp = format(float(ret["main"]["temp"]) - 273.1, '.2f')
+#         print(f"Its {temp} degrees in {loc} and {weather}")
+#     except Exception:
+#         print("error")
     
 
-main()
+# main()
+
+f = open("book.txt", 'r')
+print(f.read())
+f.close()
+
+# With open ('book.txt',"r") as document:
+# Document_Content = document.read()
+# print (document_content)
+
+With open ('book.txt, 'r'') as document:
+
+    size_to_read = 150
+    document_content = document.read(size_to_read)
+    while len(document_content) > 0:
+        print(document_content, end = '*')
+        document_content =dpcument.read(size_to_read)
+############## Example 
+
+with open("book.txt", "r") as document_read:
+    with open("copy_book.txt", 'w') as document_write:
+        for line in document_read:
+            document_write.write(line)
