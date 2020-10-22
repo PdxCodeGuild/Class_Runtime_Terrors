ba2f810544870613d09d4711299f4cdd2318f213
Ben <niu.41@osu.edu>
2020-10-22 00:37:39 -0400

    10_21 Class

--- Students/Ben/Lab_15_Count_Words/Count_Words.py
+++ Students/Ben/Lab_15_Count_Words/Count_Words.py
@@ -1,10 +1,30 @@
 # 10/21/2020
 
-# import requests
-# response = requests.get('http://www.gutenberg.org/cache/epub/22210/pg22210.txt')
-# response.encoding = 'utf-8'
-# print(response.text)
+import requests
+import string
+response = requests.get('http://www.gutenberg.org/cache/epub/22210/pg22210.txt')
+response.encoding = 'utf-8'
+book_content = response.text
 
-with open ('Forbidden_Land.txt', 'r', encoding ='utf-8') as file:
-    text = file.read()
-print(text) 
+result = string.punctuation
+print(result)
+for char in book_content:
+    if char in result:
+        char = char.replace(char,"")
+
+
+
+
+# book_content = book_content.lower() # Make everything lower case
+book_list = book_content.split(" ")
+print(book_list)
+
+
+
+
+
+
+
+# with open ("\\wsl$\Ubuntu-20.04\home\uzumakiniu\Class_Runtime_Terrors\Students\Ben\Lab_15_Count_Words\Forbidden_Land.txt","r", encoding='utf-8') as file:
+#     text = file.read()
+# print(text) 
