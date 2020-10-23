4ec6c705a5a8b84e4c6d257dcf19916263a7ef3f
Justleecho <MrEricLe@gmail.com>
2020-10-22 17:56:19 -0700

    L15 V3

--- Students/Eric/Lab 15/l15.py
+++ Students/Eric/Lab 15/l15.py
@@ -2,19 +2,19 @@ import string
 
 with open ('book.txt', encoding='utf-8') as document:
     document_content = document.read()
-    wordslist = document_content
-    wordslist = wordslist.strip()
-    wordslist = wordslist.lower()
-    wordslist = wordslist.split() 
+    wordslist = document_content #Book to Var
+    wordslist = wordslist.strip() # Remove all lines and spaces
+    wordslist = wordslist.lower() # lower case all words
+    wordslist = wordslist.split()  # split into words
     
 
-worddic = dict() 
+worddic = dict() # Eypty Dictionary
 
-for x in wordslist: 
-    if x in worddic: 
+for x in wordslist: #loop all the workds
+    if x in worddic: #Key in the diction plus one
         worddic[x] = worddic[x] + 1
     else: 
-        worddic[x] = 1
+        worddic[x] = 1 # Make new key
 
 
 worddic =sorted(worddic.items(), key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
