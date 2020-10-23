e77fbb5d68c3f68a89d3d3c52db7ba7c46d0a72a
CadillacJack42 <72709991+CadillacJack42@users.noreply.github.com>
2020-10-23 13:14:09 -0700

    Added Comments

--- Students/cadillacjack/lab15/raven.py
+++ Students/cadillacjack/lab15/raven.py
@@ -1,15 +1,30 @@
+'''
+Written by James Keicher
+This is a Cadillac Jack production
+Completed 10/23/20
+'''
+
 import requests
 from string import punctuation as punct
+# Importing modules needed for code
 
 response = requests.get('https://www.gutenberg.org/files/45484/45484-0.txt')
 response.encoding = 'utf-8' # set encoding to utf-8
+# Pull book from website "gutenberg.org"
+# Encode book into utf-8 format so we can manipulate it
 
 poe = response.text
+# Save contents of book to variable "poe"
 
 unwanted = ("\n \r \t" + punct)
+# Create variable with escape characters and punctuation
+
 poe = poe.strip(unwanted).lower().split(" ")
+# Remove all punction and escape characters,
+# Lowercase every word, Separate each word with space
 
 raven = {}
+# Create blank dictionary
 
 for i in poe:
     if i == "":
@@ -18,9 +33,16 @@ for i in poe:
         raven[i] = 1
     elif i in raven:
         raven[i] += 1
+# Iterate through book, after skipping blank strings,
+# Check if a word is in "raven", if word in "raven",
+# Add 1 to value of word, if not in "raven", 
+# Add to "raven" with value of 1
 
 raven = list(raven.items())
+# Change dictionary "raven" into a list of tuples
 
 raven.sort(key = lambda tup: tup[1], reverse = True)
+# Sort "raven" by index, reorder high to low
 
 print(raven[:10])
+# Print out top 10 words in Edgar Allen Poe's "The Raven"
