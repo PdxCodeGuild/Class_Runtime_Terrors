9fbcd71b8b9a005ce3dc34503d40a0b54a2e463b
Justleecho <MrEricLe@gmail.com>
2020-10-22 17:52:45 -0700

    L15 V3

--- Students/Eric/Lab 15/l15.py
+++ Students/Eric/Lab 15/l15.py
@@ -1,3 +1,22 @@
-with open ('book.txt', 'r') as document:
+import string
+
+with open ('book.txt', encoding='utf-8') as document:
     document_content = document.read()
-    print(document_content) 
+    wordslist = document_content
+    wordslist = wordslist.strip()
+    wordslist = wordslist.lower()
+    wordslist = wordslist.split() 
+    
+
+worddic = dict() 
+
+for x in wordslist: 
+    if x in worddic: 
+        worddic[x] = worddic[x] + 1
+    else: 
+        worddic[x] = 1
+
+
+worddic =sorted(worddic.items(), key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
+for i in range(min(10, len(worddic))):  # print the top 10 words, or all of them, whichever is smaller
+    print(worddic[i])
