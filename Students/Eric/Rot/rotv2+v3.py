0d7a3752ffacf2a231f174a1d783fb6ab11a7195
Justleecho <MrEricLe@gmail.com>
2020-10-23 19:14:05 -0700

    Rot v2 + v3 Updated with Comments

--- Students/Eric/Rot/rotv2+v3.py
+++ Students/Eric/Rot/rotv2+v3.py
@@ -4,25 +4,25 @@ def rot(word,rot):
     rot = rot
     for x in range(len(wordlist)):
         newasc = ord(wordlist[x]) #Char to ascii valve
-        if (newasc >= 97 and newasc <=122) or (newasc >= 65 and newasc <=90):
-                if (newasc >= 97 and newasc <=122):
+        if (newasc >= 97 and newasc <=122) or (newasc >= 65 and newasc <=90): # Check for A - Z and a - z
+                if (newasc >= 97 and newasc <=122): # Check from a - z
                     if ((newasc + rot) >= 123 ):
                         newasc -= rot
                         answerlist.append(chr(newasc))
                     else:
                         newasc += rot
                         answerlist.append(chr(newasc))
-                else:
+                else: # Check A - Z
                     if ((newasc + rot) >= 91):  
                         newasc -= rot
                         answerlist.append(chr(newasc))
                     else:
                         newasc += rot
                         answerlist.append(chr(newasc))
-        else:
+        else: # Pass all other Punuction without Rot
             answerlist.append(chr(newasc))
 
-    return answerlist #Return ascii valve to char
+    return answerlist
 
 print("Welcome to the Rot Cipher")
 userinput = input('Input your password to be cipher: ')
