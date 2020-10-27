8fe855f7482d8d92c0b48db8555f9951c391e103
j15527 <mlastof@gmail.com>
2020-10-26 22:15:47 -0500

    Python classes bank account

---
+++ Students/andrew/OOP/Bankaccount.py
@@ -0,0 +1,45 @@
+import datetime
+
+
+# I wouldn't round money in the real world, just to make the formatting nicer.
+
+class BankAccount:
+    def __init__(self, name, account_number, balance):
+        self.name = name
+        self.account_number = account_number
+        self.balance = balance
+        self.time = datetime.datetime.today().ctime()
+
+    def deposit(self, depo):
+        msg = "Deposit Screen"
+        print(f"{msg:-^30}")
+        self.balance += depo
+        self.balance -= self.bank_fee()
+        print(f'You have deposited ${depo}. \n'
+              f'Your new balance is ${round(self.balance)}')
+        print(f'Transaction completed on {self.time}')
+
+    def withdrawal(self, amount):
+        msg = "Withdrawal Screen"
+        print(f"{msg:-^30}")
+        self.balance -= amount
+        self.balance -= self.bank_fee()
+        print(f'You have withdrawn ${amount} \n'
+              f'Your new balance is: ${round(self.balance)}')
+        print(f'Transaction completed on {self.time}')
+
+    def display(self):
+        msg = "Account Infomation"
+        print(f"{msg:-^30}")
+        print(f'Account Holder: {self.name}\n'
+              f'Current balance: ${round(self.balance)}')
+
+    def bank_fee(self):
+        # msg = "Your fees"
+        # print(f"{msg:-^30}")
+        fee = 0.05
+        current_balance = self.balance
+        fee_charge = (current_balance * 0.05)
+        print(f'Your 5% fee for this transaction is ${round(fee_charge)}')
+        fee_charge = (current_balance * 0.05)
+        return fee_charge
