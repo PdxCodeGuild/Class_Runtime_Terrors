fac1378570c67b34a055525080011663d93131e0
Tom <thomas.p.schroeder@gmail.com>
2020-10-26 19:20:50 -0700

    Bank Account class practice

---
+++ Students/tom/OOP/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def__init__(account_info, accountnumber, name, balance):
+        account_info.accountnumber = number
+        account_info.name = name
+        account_info.balance = balance
+    def deposit(account_info):
+        return account_info.deposit
+    
+    def bankfees(account_info):
+        return account_info.bankfees
+    
+    def display(account_info):
+        return account_info.display
+
+
