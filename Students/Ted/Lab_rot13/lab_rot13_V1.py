0c9f721a2270a959711ef009dc1a7b13c26554b7
Ted Ooghe <tedaeo@gmail.com>
2020-10-27 20:52:08 -0700

    stuff

--- Students/Ted/Lab_rot13/lab_rot13_V1.py
+++ Students/Ted/Lab_rot13/lab_rot13_V1.py
@@ -1,7 +1,7 @@
 alpha = 'abcdefghijklmnopqrstuvwxyz'
+#rotation = int(input('How much rotation do you want in your encription? '))
 user_word = input('What word do you wish to encode? ').lower()
-#print(user_word.split(','))
-#word = user_word
+
 inword = []
 for letter in user_word:
     inword.append (letter)
@@ -13,19 +13,10 @@ for letter in alpha:
 answer = ''
 for i in inword:
     indexs_a = alpha.find(i)
-    indexs_b = indexs_a + 13
+    indexs_b = indexs_a + 13  #rotation
     if indexs_b > 25:
         indexs_b -= 26
     new_letter = alpha[indexs_b]
     answer = answer + new_letter
 print(f'Your encoded answer is: {answer}')
    
-    
-
-#print(user_word)
-
-
-
-
-
-#print(coded_word)
