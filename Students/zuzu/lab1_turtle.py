201e0128ec0fa37f69d480925a82d82737597858
MichéLozano <elmiche@latinooutdoors.org>
2020-11-16 20:52:23 -0800

    :recycle:

---
+++ Students/zuzu/lab1_turtle.py
@@ -0,0 +1,16 @@
+#lab 1 turtle
+from turtle import *
+fillcolor('green')
+begin_fill()
+
+edge_length = 500
+no_face = 8
+
+i = 0
+while i < no_face:
+	forward(edge_length/no_face)
+	right(360/no_face)
+	i = i + 1
+
+end_fill()
+done()
