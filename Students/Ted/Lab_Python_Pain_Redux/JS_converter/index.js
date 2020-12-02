d9133fbf289f903e7495d926f3598cd59ca18852
Ted Ooghe <tedaeo@gmail.com>
2020-12-02 00:30:02 -0800

    forth version

--- Students/Ted/Lab_Python_Pain_Redux/JS_converter/index.js
+++ Students/Ted/Lab_Python_Pain_Redux/JS_converter/index.js
@@ -5,14 +5,14 @@ const inch = 0.0254;
 const yard = 0.9144;
 const kilometers = 1000;
 const miles = 1609.34;
-const meters = 0.3048;
-function lengthToMeters (){
-    // let choices = {'feet', 'miles', 'kilometers'};
+const feet = 0.3048;
+function lengthToLength (){
     let conversion = 0;
-    let unit = readline.question('What units is the length in? The choices are: feet, miles, inches, yards or kilometers: ');
-    let length = parseFloat(readline.question('What length, including decimals, do you wish to convert to meters?: '));
+    let unit = readline.question('What unit do you wish to convert from? The choices are: feet, miles, inches, yards, meters or kilometers: ');
+    let unitout = readline.question('What unit do you wish to convert too? The choices are: feet, miles, inches, yards, meters or kilometers: ');
+    let length = parseFloat(readline.question('What length, including decimals, do you wish to convert?: '));
     if (unit == 'feet'){
-        conversion = length * meters;
+        conversion = length * feet;
     }
     else if (unit == 'miles'){
         conversion = length * miles;
@@ -26,9 +26,31 @@ function lengthToMeters (){
     else if (unit == 'yards'){
         conversion = length * yard;
     }
-    // else if unit not in choices{} 
+    else if (unit == 'meters'){
+        conversion = length
+    }
+    //converting to other than meters
+    if (unitout == 'feet'){
+        conversion = conversion / feet;
+    }
+    else if (unitout == 'miles'){
+        conversion = conversion / miles;
+    }
+    else if (unitout == 'kilometers'){
+        conversion = conversion / kilometers;
+    }
+    else if (unitout == 'inches'){
+        conversion = conversion / inch;
+    }
+    else if (unitout == 'yards'){
+        conversion = conversion / yard;
+    }
+    else if (unitout == 'meters'){
+        conversion = conversion 
+    }
+    
     let answer = conversion.toFixed(3);
-    console.log(`${length} ${unit} is equal to ${answer} meters.`);
+    console.log(`${length} ${unit} is equal to ${answer} ${unitout}.`);
 }
-lengthToMeters()
+lengthToLength()
 //to get terminal to run this: node index.js
