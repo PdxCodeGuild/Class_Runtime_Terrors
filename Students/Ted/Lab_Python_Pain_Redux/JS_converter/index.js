3b478d10ee6e0ee5fa7c6779b5bfd714c7885dcf
Ted Ooghe <tedaeo@gmail.com>
2020-12-01 21:40:43 -0800

    Third version

--- Students/Ted/Lab_Python_Pain_Redux/JS_converter/index.js
+++ Students/Ted/Lab_Python_Pain_Redux/JS_converter/index.js
@@ -1,14 +1,15 @@
 const readline = require('readline-sync');
 // let userInput = parseInt(readline.question('enter a number: '));
 // console.log(`you entered: ${userInput}`);
-
+const inch = 0.0254;
+const yard = 0.9144;
 const kilometers = 1000;
 const miles = 1609.34;
 const meters = 0.3048;
 function lengthToMeters (){
     // let choices = {'feet', 'miles', 'kilometers'};
     let conversion = 0;
-    let unit = readline.question('What units is the length in? The choices are: feet, miles or kilometers: ');
+    let unit = readline.question('What units is the length in? The choices are: feet, miles, inches, yards or kilometers: ');
     let length = parseFloat(readline.question('What length, including decimals, do you wish to convert to meters?: '));
     if (unit == 'feet'){
         conversion = length * meters;
@@ -19,10 +20,15 @@ function lengthToMeters (){
     else if (unit == 'kilometers'){
         conversion = length * kilometers;
     }
+    else if (unit == 'inches'){
+        conversion = length * inch;
+    }
+    else if (unit == 'yards'){
+        conversion = length * yard;
+    }
     // else if unit not in choices{} 
     let answer = conversion.toFixed(3);
     console.log(`${length} ${unit} is equal to ${answer} meters.`);
-
 }
 lengthToMeters()
 //to get terminal to run this: node index.js
