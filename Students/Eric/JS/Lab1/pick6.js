5d210759d42200622c8af8b027e9b0de15e98ca0
Justleecho <MrEricLe@gmail.com>
2020-12-01 20:57:54 -0800

    pick6.js

--- Students/Eric/JS/Lab1/pick6.js
+++ Students/Eric/JS/Lab1/pick6.js
@@ -1,37 +1,85 @@
-const readline = require('readline-sync');
 
-let userdis = parseInt(readline.question('What is your distance? '));
-let userunitin = readline.question('What are your input units? ');
-let userunitout = readline.question('What are your output units? ');
-let base = ""
-let out = ""
+function getRandomInt(max) 
+    {
+    return Math.floor(Math.random() * max) + 1;  // returns a random integer from 1 to 99
+    }
+  
+function pick66() 
+    {
+    let pick6 = [];
+    for (let i = 0; i < 6; i++) 
+        {
+        pick6.push(getRandomInt(99));
+        }
+    return pick6
+    }
 
-if (userunitin === 'ft'){
-    base = userdis * 0.3048
-}else if (userunitin === 'mi'){
-    base = userdis * 1609.34
-}else if(userunitin === 'm'){
-    base = userdis * 1
-}else if(userunitin === 'km'){
-    base = userdis * 1000
-}else if(userunitin === 'yd'){
-    base = userdis * 0.9144
-}else if(userunitin === 'in'){
-    base = userdis * 0.0254
-}    
 
-if (userunitout === 'ft'){
-    out = base * 1/0.3048
-}else if (userunitout === 'mi'){
-    out = base * 1/1609.34
-}else if(userunitout === 'm'){
-    out = base * 1/1
-}else if(userunitout === 'km'){
-    out = base * 1/1000
-}else if(userunitout === 'yd'){
-    out = base * 1/0.9144
-}else if(userunitout === 'in'){
-    out = base * 1/0.0254
-}    
+function num_matches(wpick6, lpick6)
+    {
+    let match = 0;
 
-console.log("Your answer is : "+ out + " " + userunitout);
+    for(let i = 0; i < 6; i++) 
+        {
+        if (wpick6[i] === lpick6[i])
+            {
+            match = match + 1;
+            }       
+        }
+    return match 
+    }
+
+function winning(matchreturn)
+    {
+    if (matchreturn === 1)
+        {
+        return 4
+        }
+    else if (matchreturn === 2)
+        {
+        return 7
+        }
+    else if (matchreturn === 3)
+        {
+        return 100
+        }
+    else if (matchreturn === 4)
+        {
+        return 50000
+        }
+    else if (matchreturn === 5)
+        {
+        return 1000000
+        }
+    else if(matchreturn === 6)
+        {
+        return 25000000
+        }
+    else
+        {
+        return 0    
+        }
+    
+    }
+
+let startmoney = 0;
+let winmoney = 0;
+let wpick6 = pick66();
+for (let i = 0; i < 100000; i++) 
+    {
+    startmoney = startmoney + 2;
+    lpick6 = pick66();
+    let match = (num_matches(wpick6,lpick6));
+    winmoney = winmoney + winning(match)
+    }
+
+let roi = ((winmoney - startmoney)/startmoney)*100
+roi = roi.toFixed(5);
+
+console.log("You just bought 100,000 Tickets at 2 bucks each!")  
+
+console.log("You spent: "+startmoney);
+console.log("You won: "+winmoney);
+
+console.log("For a ROI of : "+roi+"%");
+
