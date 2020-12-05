5a0530f63c0a6dd218fd77e402966294eb813c22
Tom <thomas.p.schroeder@gmail.com>
2020-12-04 20:58:12 -0800

    corrected pick6

--- Students/tom/js/pick6/index.js
+++ Students/tom/js/pick6/index.js
@@ -1,7 +1,6 @@
 const readline = require('readline-sync');
 winningPicks = []
 
-let loop1
 for (loop1 = 0; loop1 < 6; loop1++) {
     num = Math.floor(Math.random() * 100)
     winningPicks.push(num)
@@ -9,44 +8,54 @@ for (loop1 = 0; loop1 < 6; loop1++) {
 
 console.log(`\nThe winning picks are  ${winningPicks}`)
 
-winningsBalance = 0
+winnings = 0
+expenses = 0
+oneMatch = 0
+twoMatch = 0
+threeMatch = 0
+fourMatch = 0
+fiveMatch = 0
+sixMatch = 0
 
-let loop2
-let loop3
-// let matches = 0
 
 for (loop2 = 0; loop2 < 100000; loop2++) {  //100,000 time loop
     playerPicks = []
-    for (loop3 = 0; loop3 < 6; loop3++){
+    for (loop3 = 0; loop3 < 6; loop3++){  //loops 6 times to pick player's numbers
         num = Math.floor(Math.random() * 100)
         playerPicks.push(num)
     }
-    // console.log(`\nThe player's picks are ${playerPicks}`)
-    winningsBalance = winningsBalance - 2
+    expenses = expenses - 2
 
     matches = 0
-    for (loop3 = 0; loop3 < 6; loop3++) {
+    for (loop3 = 0; loop3 < 6; loop3++) {  //loops 6 times to compare player's numbers with winning number
         if (playerPicks [loop3] == winningPicks [loop3]) {
             matches++
-            // if (matches > 1){
-                // console.log(`\nMatches are ${matches}`)
-            // }
         }
     }
 
+
     if (matches == 1) {
-        winningsBalance = winningsBalance + 4
+        winnings = winnings + 4
+        oneMatch++
     }else if (matches == 2) {
-        winningsBalance = winningsBalance + 7
+        winnings = winnings + 7
+        twoMatch++
     }else if (matches == 3) {
-        winningsBalance = winningsBalance + 100
+        winnings = winnings + 100
+        threeMatch++
     }else if (matches == 4) {
-        winningsBalance = winningsBalance + 50000
+        winnings = winnings + 50000
+        fourMatch
     }else if (matches == 5) {
-        winningsBalance = winningsBalance + 1000000
+        winnings = winnings + 1000000
+        fiveMatch++
     }else if (matches == 6) {
-        winningsBalance = winningsBalance + 25000000
+        winnings = winnings + 25000000
+        sixMatch++
     }
 }
 
-console.log(`\nYour winnings are $${winningsBalance}.`)
+console.log(`\nYour winnings are $${winnings}.`)
+console.log(`\nYour expenses are $${expenses}.`)
+console.log(`\nYou had ${oneMatch} one match, ${twoMatch} two match, ${threeMatch} three match, ${fourMatch} four match, ${fiveMatch} five match, and ${sixMatch} six match.`)
+console.log(`\nYour RTI is ${(winnings - expenses)/expenses}.`)
