53abb84ae34a07cde908e8885706bc7a7398f825
Ted Ooghe <tedaeo@gmail.com>
2020-12-09 20:38:36 -0800

    some functionality that I want to save

--- Students/Ted/Todo_dom/app.js
+++ Students/Ted/Todo_dom/app.js
@@ -1,21 +1,38 @@
 let field = document.getElementById('input_field');
-let output =document.getElementById('output');
-let trigger = document.getElementById('trigger');
-
-trigger.addEventListener('click', function(){
-    console.log(text_field.value)
-})
-
+let output = document.getElementById('output');
 let add = document.getElementById('add');
-let tap = document.getElementById('add');
-tap.addEventListener('click', function(){
-    want = document.createElement('li');
-    want.appendChild(field.value);
+let todo = document.getElementById('todo');
+let removeItem = document.getElementById('delete');
+let done = document.getElementById('done');
+
+add.addEventListener('click', function(){
+    
+    let bullet = document.createElement('li');
+    bullet.innerText = field.value;
+    // let todo = document.getElementById('todo');
+    todo.appendChild(bullet);
+    field.value = '';
+
 })
+removeItem.addEventListener('click', function(){
+    todo.removeChild(todo.lastElementChild)
+})
+todo.addEventListener('click', function(){
+    let dot = document.querySelector('#todo > li');
+    done.appendChild(dot);
+})
+
+
+
+// let tap = document.getElementById('add');
+// tap.addEventListener('click', function(){
+//     want = document.createElement('li');
+//     console.log(want.appendChild(output.innerHTML));
+    
+// })
 
 
 let remove = document.getElementById('delete');
 
 let undone = document.getElementById('todo');
 
-let done = document.getElementById('done');
