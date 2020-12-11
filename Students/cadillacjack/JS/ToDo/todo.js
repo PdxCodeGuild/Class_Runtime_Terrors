5e1af7d3fcc7f4753c652378ab7c4b6d9cebbc60
James Keicher <jamesdkeicher@gmail.com>
2020-12-10 23:13:26 -0800

    Corrected file path

--- Students/cadillacjack/JS/ToDo/todo.js
+++ Students/cadillacjack/JS/ToDo/todo.js
@@ -1,48 +1,50 @@
-let a = document.getElementById('btn-1');
-let b = document.getElementById('btn-2');
-let c = document.getElementById('btn-3');
+let addIt = document.getElementById('btn-1');
+let moveIt = document.getElementById('btn-2');
+let removeIt = document.getElementById('btn-3');
 
 let incomplete = document.getElementById('toDoList')
 let complete = document.getElementById('complete')
 let text = document.getElementById('text-field')
 
-a.addEventListener('click',function(){
+addIt.addEventListener('click',function(){
     const li = document.createElement('li')
     li.style.listStyle = "none";
     const check = document.createElement('input');
-    check.setAttribute('type', 'checkbox', 'value', text.value);
+    check.setAttribute('type', 'checkbox');
     let doit = incomplete.appendChild(li);
-    let checkit = li.appendChild(check)
-    doit.innerHTML += text.value;
+    let checkit = li.append(check)
+    doit.innerHTML += ' ' + text.value;
     
     
-})
-
-b.addEventListener('click',function(){
-    const li = document.createElement('li')
-    li.style.listStyle = "none";
-    let doitb = complete.appendChild(li);
-    console.log("work?")
-    doitb.innerHTML += `
-    <img class=checkPic width=35px src='media/checkbox.jpg'>
-    ${text.value}`;
-    
-    
-})
-c.addEventListener('click',function(){
-    let ol = document.getElementById('toDoList')
-    let li = ol.children
-    console.log(ol)
-    for(let i=0; i<li.length; i++){
-        if (li[i].children[0].checked){
-            let trash = li[i]
-            trash.innerHTML = ''
-            console.log(trash)
+    removeIt.addEventListener('click',function(){
+        for(let i=0; i<incomplete.children.length; i++){
+            if (li.children[0].checked){
+                li.children[0].checked=false;
+                console.log('Make Money Money')
+                incomplete.removeChild(li) 
+                console.log(incomplete)
+            }
         }
-    }
-
-
-    // let checkit = li.splice(check)
-    // let doit = incomplete.splice(li);
-    // doit.innerHTML += text.value;
+    })
+    moveIt.addEventListener('click',function(){
+        for(let i=0; i<incomplete.children.length; i++){
+            if (li.children[0].checked){
+                li.children[0].checked=false;
+                const lis = document.createElement('li')
+                lis.style.listStyle = "none";
+                let doitb = complete.appendChild(lis);
+                console.log("work?")
+                doitb.innerHTML += `
+                <img class=checkPic width=35px src='media/checkbox.jpg'>
+                ${li.innerText}`;
+                incomplete.removeChild(li)
+            }
+        }
+    })
 })
+
+
+
+
+
+
