edd2e30c6933e0c824189421dad2d30e8007ad55
Tom <thomas.p.schroeder@gmail.com>
2020-12-10 19:23:26 -0800

    TODO List

--- Students/tom/js/Todo List/index.js
+++ Students/tom/js/Todo List/index.js
@@ -1,10 +1,10 @@
 let newItem         = document.getElementById('newItem')
 let addToList       = document.getElementById('add-to-list')
 let removeFromList  = document.getElementById('remove-from-list')
-let taskList = document.getElementById('incompleteTasks')
+let incompleteTasks = document.getElementById('incompleteTasks')
+let completedTasks  = document.getElementById('completedTasks')
 
 addToList.addEventListener('click', function(){
-    console.log(newItem.value)
     addedItem = document.createElement('li') //create list item
     itemText = document.createTextNode(newItem.value) //create a node for the text
     addedItem.appendChild(itemText) //add text to the list item
@@ -12,37 +12,16 @@ addToList.addEventListener('click', function(){
 })
 
 removeFromList.addEventListener('click', function(){  //this is for the remove button
-    taskList.removeChild(taskList.lastElementChild)
+    incompleteTasks.removeChild(incompleteTasks.lastElementChild)
 })
 
-// move from incomplete to complete
-tasks = document.getElementById('incompleteTasks').getElementsByTagName('li')
-for (let i = 0; i < tasks.length; i++){
-    tasks[i].addEventListener('click', function(){
-        // addedItem = tasks[i].outerHTML
-        // console.log(addedItem)
-        // document.getElementById('completedTasks').appendChild(addedItem) //addes item to list
-        
-        
-        addedItem = document.createElement('li') //create list item
-        itemText = document.createTextNode(tasks[i].outerText) //create a node for the text
-        console.log(tasks)
-        addedItem.appendChild(itemText) //add text to the list item
-        document.getElementById('completedTasks').appendChild(addedItem) //addes item to list
-        taskList.removeChild(tasks[i])
-        console.log(tasks)
-    })
-}
 
-// move from complete to incomplete
-// taskList = document.getElementById('completedTasks')
-// tasks = document.getElementById('completedTasks').getElementsByTagName('li')
-// for (let i = 0; i < tasks.length; i ++){
-//     tasks[i].addEventListener('click', function(){
-//         addedItem = document.createElement('li') //create list item
-//         itemText = document.createTextNode(tasks[i].outerText) //create a node for the text
-//         addedItem.appendChild(itemText) //add text to the list item
-//         document.getElementById('incompletTasks').appendChild(addedItem) //addes item to list
-//         taskList.removeChild(tasks[i])
-//     })
-// }
+// move from incomplete to complete
+incompleteTasks.addEventListener('click', function(event){
+    document.getElementById('completedTasks').appendChild(event.target)
+})
+
+// move from complete to incomplete 
+completedTasks.addEventListener('click', function(event){
+    document.getElementById('incompleteTasks').appendChild(event.target)
+})
