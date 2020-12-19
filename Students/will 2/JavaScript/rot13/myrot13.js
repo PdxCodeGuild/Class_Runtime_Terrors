c61057c7e175a2eed0f39b0d472e351f4214bfc0
unknown <wktc.com@gmail.com>
2020-12-19 00:42:54 -0500

    end of day rot13 update

--- Students/will 2/JavaScript/rot13/myrot13.js
+++ Students/will 2/JavaScript/rot13/myrot13.js
@@ -1,9 +1,13 @@
 const readline = require('readline-sync');
-let unenOrEn = readline.question('encrypting? y/n ');
+
 let unencryptedMsg = readline.question('enter a string: ');
 let letters = 'abcdefghijklmnopqrstuvwxyz';
-let rotation = readline.question('enter the rotation: ')
-rotation = rotation % 26
+let rotation = Math.abs(parseInt(readline.question('enter the rotation: ')))
+if (rotation > 26){
+    rotation = rotation % 26;
+} else {
+    rotation = rotation // breaks here-ish...
+}
 let upperLetters = letters.toUpperCase();
 encryptedArray = [];
 
