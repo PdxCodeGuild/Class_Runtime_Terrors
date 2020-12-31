956682ac1774f74778165c847cbde9e7ce15a64a
Alexander De Michieli <ademichieli@squarespace.com>
2020-12-31 11:44:30 -0800

    saving

--- Django/Solutions/Library/book_app/models.py
+++ Django/Solutions/Library/book_app/models.py
@@ -20,8 +20,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True)
-    
+    author = models.ForeignKey(Author, on_delete=models.PROTECT, blank=True)
     def __str__(self):
         return "%s %s" % (self.title, self.author)
 
@@ -30,8 +29,8 @@ class LandBook(models.Model):
         ('yes','Book is out'),
         ('no','Book is in'),
     )
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, blank=True)
-    user = models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.PROTECT, blank=True)
+    user = models.ForeignKey(User, on_delete = models.PROTECT, null=True, blank=True)
     status = models.CharField(max_length = 200, choices = out, blank = True)
     timestamp = models.DateTimeField(default=datetime.datetime.now)
 
