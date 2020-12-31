653e7b4f7b043d3c1af653804f9e120b92537bc8
Andrew Babcock <mlastof@gmail.com>
2020-12-30 21:56:02 -0600

    jquery todo complete

--- Students/andrew/js/lab2/js/jqapp.js
+++ Students/andrew/js/lab2/js/jqapp.js
@@ -1,23 +1,37 @@
-$('#add-btn').click(function (){
-    
+$('#add-btn').click(function () {
+
     let text = $('#inputfield').val();
+    let div = $('<div class="inner"></div>');
     let li = $('<li></li>').text(text);
-    let clear = $('<button/>').text('clear').click(function (){
-        alert();
+    let clear = $('<button/>').text('clear').click(function () {
+        $(div).empty();
     });
-    let complete = $('<button id="comp"></button>').text("Complete").click(function (){
-        $('#complete').append(li);
+
+
+    let complete = $('<button class="comp"></button>').text("Complete").click(function () {
+        li.css('textDecoration', 'line-through');
+        $(div).append(clear);
+        $(complete).css('backgroundColor', 'red');
 
 
     });
-   
-    if (text === ""){
+
+    if (text === "") {
         alert("Please enter a TODO in the text field.")
-    }else{
-        
-        $('#todo').append(li);
-        $(li).append(complete);
+    } else {
+        $('#todo').append(div);
+        $(div).append(li);
+        $(div).append(complete);
+
     }
-    
+
 });
+
+
+
+
+$('#clear-all').click(function () {
+    $('#todo').remove();
+    location.reload();
+})
 
