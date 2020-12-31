ec4f7792bbf92db8251dee55091ab3a10d19bfd5
Ted Ooghe <tedaeo@gmail.com>
2020-12-30 20:08:37 -0800

    with CURIOSITY navigation camera

--- Students/Ted/Lab5_any_api/script.js
+++ Students/Ted/Lab5_any_api/script.js
@@ -1,25 +1,28 @@
+let marsday= '1'
 
 
-
-
-
-
-fetch('https://api.nasa.gov/planetary/apod?api_key=AqXkEe8852lvQ5SgKlAVUJwJ0b0hWoXCwzo3gDPU', {
+fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${marsday}&camera=NAVCAM&api_key=mrrK6GWgF16JAzBzCdAmfgMfl4AK6NHz2WYnW3cg`, {
     Method: 'GET',
     
 })
     .then(function(response){
         return response.json()
+
     })
 
     .then(function(data){
         console.log(data)
         let image=document.createElement('img');
-        image.src= 'data.url'
+        // let i = 0;
+        // while (i < Array.length){
+
+        // }
+        image.src= data.photos[3].img_src
+
         console.log(image)
-        console.log(data.url)
+        // console.log(data.url)
         let back = document.getElementById('wrapper')
-        back.style.backgroundImage=`url(${data.url})`
+        back.appendChild(image)
         // document.body.style.backgroundImage= data.url
         
     })
