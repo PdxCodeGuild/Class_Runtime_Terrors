510b9854bd9f9bab8c82c93666afe63517442684
Ted Ooghe <tedaeo@gmail.com>
2021-01-12 21:37:32 -0800

    fun to do a little css

--- Students/Ted/djangogirls/django_girls_blog/settings.py
+++ Students/Ted/djangogirls/django_girls_blog/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
