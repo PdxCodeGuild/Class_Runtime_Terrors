9b6a7a3f0dcc017acac52872b0e7eceda66b2718
AlexDeMichieli <56011259+AlexDeMichieli@users.noreply.github.com>
2021-01-19 18:49:22 -0800

    Delete code_guild/Class_Runtime_Terrors/Students/will directory

--- code_guild/Class_Runtime_Terrors/Students/will/JavaScript/clock (4)/scratchPad.js
+++
@@ -1,7 +0,0 @@
-
-var h = date.getHours();
-var m = date.getMinutes();
-var s = date.getSeconds();
-h = digits(h);
-m = digits(m);
-s = digits(s);
--- code_guild/Class_Runtime_Terrors/Students/will/Python/Lab_API/weather_check/weather request.py
+++
@@ -1,6 +0,0 @@
-import requests
-response = requests.get(f"https://jsonplaceholder.typicode.com/todos/1")
-data = response.json()
-print(data)
-
-pip3 install requests
