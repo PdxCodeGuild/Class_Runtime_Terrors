2ac5ba67df6f563a81cf7ce3166cb469c66f8e9a
Dwmorrison <wktc.com@gmail.com>
2021-01-20 22:48:21 -0500

    URL update

---
+++ Students/will/Django/URL_shortener/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\17196\desktop\pdx_code\class_runtime_terrors\students\will\django\url_shortener\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
