9853142a9c40063ae7ca36f8756c43ddb1ba4be2
Ted Ooghe <tedaeo@gmail.com>
2021-02-01 18:20:49 -0800

    to fix problem with git

--- Students/Ted/Quarts/quarts_project/urls.py
+++ Students/Ted/Quarts/quarts_project/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path('', include('quarts_app.urls'))
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
