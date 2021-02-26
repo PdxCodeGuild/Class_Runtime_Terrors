b1c36459ccd5338398b88cdcabc299e116c62a3a
Alexander De Michieli <ademichieli@squarespace.com>
2021-02-25 22:20:28 -0800

    save

---
+++ Django/Solutions/Simple_Blog/Blog app Django/accounts/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def user_register(request):
+    if request.method == 'POST':
+        users = User.objects.all().filter(username=request.POST['username']).exists()
+        if users:
+            messages.warning(request, 'User already exsits in the Database')
+        else:
+            new_user = User(
+                username = request.POST['username'],
+                email = request.POST['email'],
+                first_name = request.POST['first_name'],
+                last_name= request.POST['last_name']
+            )
+            new_user.set_password(request.POST['password'])
+            new_user.save()
+            messages.success(request, 'User registered!')
+            return redirect('login')
+    return render(request, 'register/register.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+        if user is not None:
+            login(request, user)
+            return redirect('posts')
+    return render(request, 'register/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
