12be68ad231f0940b4cfd69c0edbea4160c14558
Eric Le <mrericle@gmail.com>
2021-02-26 07:51:28 -0800

    To Do

--- Students/Eric/Django/todo/todo/settings.py
+++ Students/Eric/Django/todo/todo/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 
 ** Notes:
-Use https://www.youtube.com/watch?v=4RWFvXDUmjo&ab_channel=DennisIvy as the core of my help. - Eric Le
+Use https://www.youtube.com/watch?v=4RWFvXDUmjo&ab_channel=DennisIvy as the core of my help. - Eric Le 
 
 
 """
@@ -19,7 +19,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
