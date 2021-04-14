604e835367f7fd2d83475197825098eaa5678e39
Ted Ooghe <tedaeo@gmail.com>
2021-04-13 20:45:21 -0700

    incomplete ushortener

--- Students/Ted/Ushort/Ushort/urls.py
+++ Students/Ted/Ushort/Ushort/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pages_app.urls')),
     path('admin/', admin.site.urls),
     path('Ushrink/', include('Ushort_app.urls')),
 ]
--- Students/Ted/Ushort/Ushort_app/views.py
+++ Students/Ted/Ushort/Ushort_app/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.utls.crypto import get_random_string
+from . models import Ushort
+from django.contrib import messages
 
 def ushrink_list(request):
-    return HttpResponse('from ushrink list')
+    all_urls = Ushort.objects.all()
+    context = {
+        "all_urls": all_urls
+    }
 
-# Create your views here.
+    if request.method == 'GET':
+        return render(request, 'home.html',context)
+    elif request.method == 'POST':
+        url = request.POST['url']
+        for x in url:
+            if 'https://' in url:
+                messages.warning(request, "please remove 'https://' from the URL")
+                return render(request, 'home.html', context)
+            else:
+                full_url = url.split('/')
+                random = get_random_string(length=5)
+                short_url = full_url[0] + x + '///' + random
+                shortened = Ushort.objects.create(url_name = url, shortened_url = short_url)
+                return render(request, 'home.html', context)
+
+
+
+
